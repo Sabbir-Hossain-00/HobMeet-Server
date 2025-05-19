64a4979f462e78d8fda30ba551fcd49e06307f2f
Sabbir-Hossain-00 <ms7398037@gmail.com>
2025-05-20 05:40:00 +0600

    initial setup done

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = 3000 
+
+console.log(process.env)
+
+app.use(cors());
+app.use(express.json());
+
+
+
+// user: hobmeetData
+// pass: 7H4CJI2ji555q5Ex
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vlrcl7k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+async function run () {
+    try{
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    
+    } 
+
+    finally{
+
+    }
+}
+
+run().catch(console.dir)
+
+
+
+app.get('/', (req , res)=>{
+    res.send("Hello this is hobmeet server");
+});
+
+app.listen(port , ()=>{
+    console.log(`hobmeet is running on port ${port}`)
+})
