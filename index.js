7a9ee170c03096515f7fe60af861cfc8fd78dd4e
Sabbir-Hossain-00 <ms7398037@gmail.com>
2025-05-20 23:27:20 +0600

    added get methode for single element

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = 3000 
 
@@ -41,7 +41,14 @@ async function run () {
         const result = await cursor.toArray();
         res.send(result);
       });
-      
+
+      app.get("/group/:id", async(req , res)=>{
+        const id = req.params.id ;
+        const query = {_id : new ObjectId(id)}
+        const result = await groupCollection.findOne(query);
+        res.send(result);
+      })
+
 
       app.post("/groups", async(req , res)=>{
         const groupData = req.body ;
